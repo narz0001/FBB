c8041cc29326d5520f46e5a9c996e06a26408c79
GeoDragon <geolangsat@gmail.com>
2016-04-15 21:50:25 +0530

    Toolbar Testing aborted for now. Last Commit will crash.

--- app/src/main/java/com/myapps/android/fbb/DashBoard.java
+++ app/src/main/java/com/myapps/android/fbb/DashBoard.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,9 +21,9 @@ public class DashBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
         final Activity activity = this;
-
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//
+//        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
 
         instoreLocater = (Button)findViewById(R.id.instore_locater);
