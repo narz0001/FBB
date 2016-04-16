85ff9b5cde032728905b5bdb37d6777921b581d6
steric85 <aneemeh@gmail.com>
2016-04-16 21:33:20 +0530

    Dashboard Page UI done

--- app/src/main/java/com/myapps/android/fbb/DashBoard.java
+++ app/src/main/java/com/myapps/android/fbb/DashBoard.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,10 @@ public class DashBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         final Activity activity = this;
 
         instoreLocater = (Button)findViewById(R.id.instore_locater);
