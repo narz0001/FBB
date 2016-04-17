303b32e4d71b640e4c29432650688359015b5e91
GeoDragon <geolangsat@gmail.com>
2016-04-17 23:39:21 +0530

    Few Files didn't get Committed.

---
+++ app/src/main/java/com/myapps/android/fbb/MyCart.java
@@ -0,0 +1,69 @@
+package com.myapps.android.fbb;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MyCart extends AppCompatActivity {
+
+    private CartAdapter myCart;
+
+    String sampleString[] = {
+        "Pant 42 2 560 1400",
+            "Shirt 36 1 480 1200",
+            "Shoes 9 1 4800 8000"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_cart);
+
+        myCart = new CartAdapter(
+                this,new ArrayList<CartItem>());
+
+
+        ListView listView = (ListView)findViewById(R.id.cart);
+        listView.setAdapter(myCart);
+
+        GetData getData = new GetData();
+
+        getData.execute(sampleString);
+    }
+
+
+    //Database Related Work Here
+    //  Have included a sample String
+    private class GetData extends AsyncTask<String[], Void, String[][]>{
+        @Override
+        protected String[][] doInBackground(String[]... params) {
+
+            String answer[][] = new String[params[0][0].length()][5];
+
+            for(int i=0; i<params[0].length; i++){
+                String temp[] = params[0][i].split(" ");
+                for(int j=0 ; j< temp.length; j++){
+                    answer[i][j] = temp[j];
+                }
+            }
+            return answer;
+        }
+
+        @Override
+        protected void onPostExecute(String[][] strings) {
+            if(strings!=null){
+                myCart.clear();
+                CartItem cartItem;
+                for (int i=0; i<3 ; i++){
+                    cartItem = new CartItem(strings[i]);
+                    myCart.add(cartItem);
+                }
+            }else{
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/myapps/android/fbb/ScanningPage.java
@@ -0,0 +1,95 @@
+package com.myapps.android.fbb;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class ScanningPage extends AppCompatActivity {
+
+    Button typeIt,done, initScan;
+    PopupWindow popUpWindow;
+    LayoutInflater layoutInflater;
+    LinearLayout rootLinear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanning_page);
+
+        typeIt = (Button)findViewById(R.id.typeIt);
+        rootLinear = (LinearLayout)findViewById(R.id.rootLinear);
+        initScan = (Button)findViewById(R.id.initScan);
+
+        typeIt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopUp();
+            }
+        });
+
+        initScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanStart();
+            }
+        });
+
+    }
+
+    void showPopUp(){
+        layoutInflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        ViewGroup container = (ViewGroup)layoutInflater.inflate(R.layout.pop,null);
+
+        popUpWindow = new PopupWindow(container, rootLinear.getMeasuredWidth() - 300,700, true);
+        popUpWindow.showAtLocation(rootLinear, Gravity.CENTER, 0, 0);
+
+        done = (Button)container.findViewById(R.id.done);
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popUpWindow.dismiss();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Log.d("MainActivity", "Cancelled scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d("MainActivity", "Scanned");
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void scanStart(){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scan");
+        integrator.setCameraId(0);
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(false);
+        integrator.initiateScan();
+    }
+}
